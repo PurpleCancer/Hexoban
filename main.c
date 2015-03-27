4052bcfd5e459f79c23e496fcca7124084466240
Jan Bajer <jan.bajer@gmail.com>
2015-03-27 01:16:42 +0100

    Basic build
    Allegro installed? Check.
    Project created? Check.
    Display test? Check.
    Ready to code ;_;

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+
+int main(int argc, char **argv)
+{
+    ALLEGRO_DISPLAY *display = NULL;
+
+    if(!al_init()) {
+        fprintf(stderr, "failed to initialize allegro!\n");
+        return -1;
+    }
+
+    display = al_create_display(1280, 720);
+    if(!display) {
+        fprintf(stderr, "failed to create display!\n");
+        return -1;
+    }
+
+    al_clear_to_color(al_map_rgb(0,0,0));
+
+    al_flip_display();
+
+    al_rest(2.0);
+
+    al_destroy_display(display);
+
+    return 0;
+}
